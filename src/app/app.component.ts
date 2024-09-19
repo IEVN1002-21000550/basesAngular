781f6d41dea2ca8173bff30a463ac115fac262b3
Miguel Angel Gonzalez Funes <21000550@alumnos.utleon.edu.mx>
2024-09-18 20:56:31 -0600

    Primer commit miercoles 18

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,31 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  //    Ejemplo de crear una pagina desde este lado
+  //template: `
+  // <div class="container">
+  // <div class="row">
+  //   <div class="col-md-12">
+  //       <h1>Angular 2+ Tutorial</h1>
+  //   </div>
+  //   </div>
+  //   </div>
+  // `,
+  templateUrl:'./app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'basesAngular';
+doblenumero(x:number):number{
+  return x*2;
 }
+alumno={
+  nombre:'Juan',
+  edad:25,
+  curso:'Angular',
+  fechaInscrito:new Date(),
+  pago:2220
+}
+
+}
+
