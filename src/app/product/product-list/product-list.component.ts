3210a2429f1f56b7ce0ecb08a089fbe84250fd47
Miguel Angel Gonzalez Funes <21000550@alumnos.utleon.edu.mx>
2024-09-21 10:11:33 -0600

    sabado 21 formulario ejemplo1

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IProductos } from '../productos';
 
 @Component({
   selector: 'app-product-list',
@@ -8,13 +9,21 @@ import { Component } from '@angular/core';
 export class ProductListComponent {
 
   //*ngIf=true, o false
-productos:any[]=[
+
+  imageWidth:number=60;
+  imageMargin:number=2;
+  muestraImg:boolean=true;
+  listFilter:string='';
+  muestImg():void{
+    this.muestraImg=!this.muestraImg
+  }
+productos:IProductos[]=[
   {
     "productoId":1,
     "Modelo":"Sentra",
     "Descripcion":"4 puertas",
     "Precio":20000,
-    "Year":2023,
+    "Year":"febrero 2 2023",
     "Marca":"NISSAN",
     "Color":"Azul",
     "ImagenUrl":"https://th.bing.com/th/id/R.6e61cbff23d37bac659a4ef0fce86495?rik=F9YXBJ11Ymmj6g&pid=ImgRaw&r=0"
@@ -25,7 +34,7 @@ productos:any[]=[
     "Modelo":"A4",
     "Descripcion":"4 puertas",
     "Precio":20000,
-    "Year":2022,
+    "Year":"marzo 23 2022",
     "Marca":"AUDI",
     "Color":"Blanco",
     "ImagenUrl":"https://th.bing.com/th/id/R.f7c1cf9df5f4944abacfcf2b47e64c82?rik=4Tk2C0E0dO7IEg&pid=ImgRaw&r=0"
@@ -36,7 +45,7 @@ productos:any[]=[
     "Modelo":"Rio",
     "Descripcion":"4 puertas",
     "Precio":15000,
-    "Year":2020,
+    "Year":"septiembre 5 2020",
     "Marca":"KIA",
     "Color":"Azul",
     "ImagenUrl":"https://th.bing.com/th/id/OIP.3ks8xUVABp_cbYx7sksl2gAAAA?rs=1&pid=ImgDetMain"
